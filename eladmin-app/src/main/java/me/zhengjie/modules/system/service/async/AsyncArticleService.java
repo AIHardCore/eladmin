647fefade86a667364cd9df4c24d2b6c178e5315
AIHardCore <aihardcore@163.com>
2025-01-07 17:25:34 +0800

    文章阅读量异步统计

--- eladmin-app/src/main/java/me/zhengjie/modules/system/service/async/AsyncArticleService.java
+++ eladmin-app/src/main/java/me/zhengjie/modules/system/service/async/AsyncArticleService.java
@@ -33,20 +33,20 @@ public class AsyncArticleService {
             long count = articleReadingLogRepository.countByArticleAndOpenIdAndType(id,openId,type);
             if (count == 0){
                 articleReadingLogRepository.save(articleReadingLog);
-                do {
-                    num++;
-                    Article article = repository.findByIdAndEnabled(id,true).orElseGet(Article::new);
-                    ValidationUtil.isNull(article.getId(),"Article","id",id);
-                    try {
-                        repository.read(article.getId(),article.getVersion());
-                    }catch (OptimisticLockException ex){
-                        log.info("更新文章{}失败，进行第[{}]次重试...",article.getTitle());
-                        if (num >= 3){
-                            log.error("{}次重试失败，更新文章[{}]彻底失败！",article.getTitle());
-                        }
-                    }
-                }while (num >= 3);
             }
         }
+        do {
+            num++;
+            Article article = repository.findByIdAndEnabled(id,true).orElseGet(Article::new);
+            ValidationUtil.isNull(article.getId(),"Article","id",id);
+            try {
+                repository.read(article.getId(),article.getVersion());
+            }catch (OptimisticLockException ex){
+                log.info("更新文章{}失败，进行第[{}]次重试...",article.getTitle());
+                if (num >= 3){
+                    log.error("{}次重试失败，更新文章[{}]彻底失败！",article.getTitle());
+                }
+            }
+        }while (num >= 3);
     }
 }
